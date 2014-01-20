cdfc827f0de3c4c30bad4359d100a4d53726ef36
Alexander Czichelski <a.czichelski@satzmedia.de>
2014-01-20 10:53:58 +0100

    Safe outer Links, if host included

--- application/views/CONTENT/outer.php
+++ application/views/CONTENT/outer.php
@@ -1,8 +1,15 @@
 <div id="top" class="welcomeLayer">
     <div class="center-text">
+        {if {$outerLink|strstr:$smarty.server.HTTP_HOST}}
+        <h1>SAFE LINK</h1>
+        <h3>You can make sure, that this link is safe, but we do not own the following content.</h3>
+        <h4><em>{$outerLink}</em></h4>
+        <a href="{$outerLink}" class="btn btn-default btn-lg">Continue</a>
+        {else}
         <h1>ATTENTION</h1>
         <h3>You are now leaving Vision Network! <br/>We do not support and owning the following site.</h3>
         <h4><em>{$outerLink}</em></h4>
         <a href="{$outerLink}" class="btn btn-default btn-lg">Continue</a>
+        {/if}
     </div>
 </div>
