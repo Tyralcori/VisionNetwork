83bdb5913664f6cc56bf994fd039a6fbedb7b741
Alexander Czichelski <a.czichelski@satzmedia.de>
2014-01-22 11:06:45 +0100

    Default TimeZone,

--- application/controllers/launcher.php
+++ application/controllers/launcher.php
@@ -2,7 +2,8 @@
 
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-
+date_default_timezone_set('Europe/Berlin');
+        
 if (!defined('BASEPATH'))
     exit('No direct script access allowed');
 
